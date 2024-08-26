12bdc15fbe292c2904d19e1f3acea980e6d88cd6
RonyZafrir <116782428+RonyZafrir@users.noreply.github.com>
2024-08-26 17:40:29 +0300

    Update and rename msdWflights to temp.py

---
+++ temp.py
@@ -0,0 +1,91 @@
+# choosing lambda for ridge regression using 5-fold CV:
+m = data.shape[0]
+p = data.shape[1] - 1
+for i in range(data.shape[1]):
+    # standardize each column i.e. feature
+    data[:, i] = standardize(data[:, i])
+np.random.seed(130)
+data = np.random.permutation(data)
+n = 1000
+K = 5
+q = int(m / n) # num of segments that the data can be divided into
+gamma = p / n
+batch_size = n / K
+steps = 40 # amount of distinct lambdas to evaluate
+lbd_seq = np.linspace(0, 0.6, steps)
+cv_error = np.zeros((steps, K))
+
+
+'''
+cross validation
+
+for the error bar, we average over 90 = q - 10 different sub-datasets
+for the test error, we train on 1000 data points and fit on 9000 test datapoints
+'''
+for k in range(q - 10):
+    X = X[n * k: n * (k + 1), 1:]
+    Y = data[n * k: n * (k + 1), 0].reshape(n, 1)
+    # performing 5-fold CV for each of the 40 optional lambdas
+    for i in range(steps):
+        lbd = lbd_seq[i]
+        for j in range(K):
+            test_idx = np.arange(j * batch_size, (j + 1) * batch_size, 1, dtype=int)
+            X_test = X[test_idx, :] # 200x90
+            Y_test = Y[test_idx, :]
+            train_idx = list(set(np.arange(0, n, 1, dtype=int)) - set(test_idx))
+            X_train = X[train_idx, :] # 800x90
+            Y_train = Y[train_idx, :]
+            #beta_hat = np.linalg.inv(X_train.T @ X_train / (n - batch_size) + lbd * np.identity(p)) @ X_train.T @ Y_train / (n - batch_size)
+            beta_hat = np.linalg.inv(X_train.T @ X_train + lbd  * (n - batch_size) * np.identity(p)) @ X_train.T @ Y_train
+            cv_error[i, j] += np.linalg.norm(Y_test - X_test @ beta_hat) ** 2 / batch_size / (q - 10)
+
+
+lbd_cv_idx = np.argmin(np.mean(cv_error, 1))
+lbd_cv = lbd_seq[lbd_cv_idx]
+lbd_cv_debiased = lbd_cv * (K - 1) / K # the bias-corrected parameter that's proposed in the paper
+lbd_cv_debiased_idx = 0
+for i in range(steps):
+    if lbd_seq[i] >= lbd_cv_debiased:
+        lbd_cv_debiased_idx = i
+        break
+
+# test error
+X_train = data[n * (q - 10): n * (q - 9), 1:] #1000x90
+Y_train = data[n * (q - 10): n * (q - 9), 0]
+X_test = data[n * (q - 9): n * q, 1:] #9000x90
+Y_test = data[n * (q - 9): n * q, 0]
+test_error = np.zeros(steps)
+
+for i in range(steps):
+    lbd = lbd_seq[i]
+    beta_ridge = np.linalg.inv(X_train.T @ X_train + lbd * np.identity(p) * n ) @ X_train.T @ Y_train
+    test_error[i] = np.linalg.norm(Y_test - X_test @ beta_ridge) ** 2 / (9 * n)
+
+lbd_smallest_idx = np.argmin(test_error)
+lbd_smallest = lbd_seq[lbd_smallest_idx]
+
+lbd_theory = gamma
+lbd_theory_idx = 0
+for i in range(steps):
+    if lbd_seq[i] >= lbd_theory:
+        lbd_theory_idx = i
+        break
+
+# Figure 2 (left), cross-validation on the million song dataset
+lb = 0.83
+ub = 0.91
+plt.errorbar(lbd_seq, np.mean(cv_error, 1), np.sqrt(np.var(cv_error, 1)), capsize=2, label='CV error bar')
+plt.plot(lbd_seq, test_error, label='Test error')
+plt.plot(lbd_cv * np.ones(10), np.linspace(lb, ub, 10), ls='--', linewidth=3, label='CV min {:.3f}'.format(test_error[lbd_cv_idx]))
+plt.plot(lbd_cv_debiased * np.ones(10), np.linspace(lb, ub, 10), ls='-.', linewidth=3, label='Debiased CV {:.3f}'.format(test_error[lbd_cv_debiased_idx]))
+plt.plot(lbd_smallest * np.ones(10), np.linspace(lb, ub, 10), ls=':', label='Test error min {:.3f}'.format(test_error[lbd_smallest_idx]), linewidth=3)
+plt.plot(lbd_theory * np.ones(10), np.linspace(lb, ub, 10), ls=':', label='Theory {:.3f}'.format(test_error[lbd_theory_idx]), linewidth=3)
+plt.legend(fontsize=13)
+plt.grid(linestyle='dotted')
+plt.xlabel(r'$\lambda$', fontsize=13)
+plt.ylabel('CV test error', fontsize=13)
+plt.title('data CV', fontsize=13)
+plt.show()
+
+print(test_error[lbd_cv_debiased_idx], test_error[lbd_cv_idx], test_error[lbd_smallest_idx])
+print("Debias improve test error by", test_error[lbd_cv_idx] - test_error[lbd_cv_debiased_idx])
